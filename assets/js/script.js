882c37a4d054910988d28a09136bc253c45c4127
ade yoseman <ade.putra@owasp.org>
2020-10-15 10:30:55 +0700

    tes

---
+++ assets/js/script.js
@@ -0,0 +1,18 @@
+window.addEventListener("load", function() {
+
+
+/*
+	document.querySelectorAll("img").forEach(function(img) {
+		var d = document.createElement("div");
+		d.className="image-container";
+		img.parentNode.insertBefore(d, img);
+		img.parentNode.removeChild(img);
+		d.appendChild(img);
+		var t = document.createElement("span");
+		t.textContent = img.title;
+		d.appendChild(t)
+	});
+*/
+}, false);
+
+
